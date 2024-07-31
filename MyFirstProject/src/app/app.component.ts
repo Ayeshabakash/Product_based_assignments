d96fb99e95f87d162eb7d388bf89e3b98a30fddb
Ayeshabaksh <ayeshaanish04@gmail.com>
2024-07-31 10:08:38 +0530

    main

--- MyFirstProject/src/app/app.component.ts
+++ MyFirstProject/src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MyFirstProject';
+  name : string ;
+  age : number;
+  email : string;
+  textcolor : string;
+  fname : string="";
+  flag : boolean = true;
+  constructor() {
+    this.name = "Siddhika";
+    this.age = 21;
+    this.email = "ayesha@gmail.com";
+    this.textcolor = "green";
+    //this flag =true;
+  }
+  getaddition(a : number,b : number)
+  {
+    let sum = a + b;
+    return sum;
+  }
+  changename(){
+    this.name = "Ayesha"
+  }
+  changeage(){
+    this.age = 31
+  }
+  changemail(){
+    this.email = "kalamayesha63@gmail.com.com"
+  }
+  changeFlag(){
+    this.flag =!this.flag;
+  }
+  setSelectedItem(vec : string){
+    this.selectedVehicle = vec;
+  }
 }
