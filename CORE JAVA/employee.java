55277517a5a15ba522f62a5151b88797ebd603d3
Ayeshabaksh <ayeshaanish04@gmail.com>
2024-08-10 17:38:40 +0530

    main

---
+++ CORE JAVA/employee.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class employee {
+    public static void main(String[] args) {
+        int employeeId;
+        String employeeName;
+        float employeeSalary;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter employee id");
+        employeeId =sc.nextInt();
+        System.out.println("Enter employee Name");
+        employeeName =sc.next();
+        System.out.println("Enter employee Salary");
+        employeeSalary =sc.nextFloat();
+        System.out.println("Enter employee Salary :"+employeeId);
+        System.out.println("Enter employee Salary :"+employeeName);
+        System.out.println("Enter employee Salary :"+employeeSalary);
+        sc.close();
+    }
+}
---
+++ CORE JAVA/factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class factorial {
+    public static void main(String[] args) {
+        System.out.println("Enter a number:");
+        Scanner sc = new Scanner(System.in);
+        int n=1;
+        int a = sc.nextInt();
+        for(int i=2;i<=a;i++){
+          n=n*i;
+        }
+        System.out.println("Factorial of the number "+ a + " is : "+ n);
+        sc.close();
+    }
+}
